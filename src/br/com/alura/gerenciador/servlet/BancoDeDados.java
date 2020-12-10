96c8c0dbac8d6aa7719d5011db88fefd107f1615
Alice <am171696@alunos.unisanta.br>
2020-12-10 18:01:25 -0300

    Adicionando bloco static que adiociona 2 empresas na lista de empresas

--- src/br/com/alura/gerenciador/servlet/BancoDeDados.java
+++ src/br/com/alura/gerenciador/servlet/BancoDeDados.java
@@ -8,6 +8,17 @@ public class BancoDeDados {
 
 	private static List<Empresa> empresas = new ArrayList<>();
 	
+	static {
+		Empresa alura = new Empresa();
+		alura.setNome("Alura");
+		Empresa caelum = new Empresa();
+		caelum.setNome("Caelum");
+		
+		BancoDeDados.empresas.add(alura);
+		BancoDeDados.empresas.add(caelum);
+		
+	}
+	
 	public void adiciona(Empresa empresa) {
 		BancoDeDados.empresas.add(empresa);
 	}
