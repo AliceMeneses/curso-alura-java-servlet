22bcdde11c31629d796e4d32b726815de27428a0
Alice <am171696@alunos.unisanta.br>
2020-12-10 18:03:25 -0300

    Adicionando servlet que lista as empresas cadastradas

---
+++ src/br/com/alura/gerenciador/servlet/ListaEmpresas.java
@@ -0,0 +1,33 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/listaEmpresas")
+public class ListaEmpresas extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BancoDeDados bancoDeDados = new BancoDeDados();
+		List<Empresa> empresas = bancoDeDados.getEmpresas();
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<ul>");
+		empresas.forEach((empresa) -> out.println("<li>" + empresa.getNome() + "</li>"));
+		out.println("</ul>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
