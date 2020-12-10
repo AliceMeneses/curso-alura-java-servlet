df26b4ac1a9de3c8566f4271ad769b4670c5ede5
Alice <am171696@alunos.unisanta.br>
2020-12-10 11:40:40 -0300

    Adicionando classe

---
+++ src/br/com/alura/gerenciador/servlet/Empresa.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.servlet;
+
+public class Empresa {
+	
+	private int id;
+	private String empresa;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getEmpresa() {
+		return empresa;
+	}
+	
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	
+	
+}
