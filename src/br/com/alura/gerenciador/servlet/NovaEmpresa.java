4532950edccf7a2220269c7b6a7c7ade7f1590f1
Alice <am171696@alunos.unisanta.br>
2020-12-11 08:48:49 -0300

    Despachando requisicao para o JSP para o JSP devolver a resposta

--- src/br/com/alura/gerenciador/servlet/NovaEmpresa.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresa.java
@@ -1,8 +1,8 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,8 +24,9 @@ public class NovaEmpresa extends HttpServlet {
 		BancoDeDados bancoDeDados = new BancoDeDados();
 		bancoDeDados.adiciona(empresa);
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html> <body> <p>Empresa " + nomeEmpresa + " cadastrada com sucesso!</p> </body> </html>");
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("nomeEmpresa", empresa.getNome());
+		rd.forward(request, response);
 	}
 
 }
