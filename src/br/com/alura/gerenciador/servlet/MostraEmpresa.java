6e1aa447961c7b2d480d79c3d4aab19132127bbe
Alice <am171696@alunos.unisanta.br>
2020-12-13 09:04:03 -0300

    Adicionando servlet que obtem a empresa requerida do bancoDeDados

---
+++ src/br/com/alura/gerenciador/servlet/MostraEmpresa.java
@@ -0,0 +1,29 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/mostraEmpresa")
+public class MostraEmpresa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String id = request.getParameter("id");
+		
+		BancoDeDados bancoDeDados = new BancoDeDados();
+		Empresa empresa = bancoDeDados.busca(Integer.valueOf(id));
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/formAlteraEmpresa.jsp");
+		request.setAttribute("empresa", empresa);
+		rd.forward(request, response);		
+	}
+
+}
