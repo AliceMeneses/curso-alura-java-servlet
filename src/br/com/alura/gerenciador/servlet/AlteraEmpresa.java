2befa419716c6928f7ac6fa04aec100254b616df
Alice <am171696@alunos.unisanta.br>
2020-12-13 11:17:34 -0300

    Adicionando servlet que faz o redirecionamento no lado do cliente, e esse servlet pede para o bancoDeDados atualizar uma empresa

---
+++ src/br/com/alura/gerenciador/servlet/AlteraEmpresa.java
@@ -0,0 +1,42 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/alteraEmpresa")
+public class AlteraEmpresa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Cadastrando empresa");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		String paramDataAbertura = request.getParameter("data");
+		String id = request.getParameter("id");
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");	
+	        dataAbertura = sdf.parse(paramDataAbertura);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		BancoDeDados bancoDeDados = new BancoDeDados();
+		bancoDeDados.atualiza(nomeEmpresa, dataAbertura, Integer.valueOf(id));
+				
+		response.sendRedirect("listaEmpresas");
+		
+	}
+
+}
