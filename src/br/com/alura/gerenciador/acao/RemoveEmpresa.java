bdf408373d1e34963d02c3dde699733e57adbe5a
Alice <am171696@alunos.unisanta.br>
2020-12-15 09:59:23 -0300

    Adicionando classe, acao RemoveEmpresa que possui o mesmo proposito que o servlet RemoveEmpresa

---
+++ src/br/com/alura/gerenciador/acao/RemoveEmpresa.java
@@ -0,0 +1,23 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.BancoDeDados;
+
+public class RemoveEmpresa {
+	
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String id = request.getParameter("id");
+		
+		BancoDeDados bancoDeDados = new BancoDeDados();
+		bancoDeDados.remove(Integer.valueOf(id));
+		
+		response.sendRedirect("listaEmpresas");
+	}
+
+}
