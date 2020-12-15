e417ee20e3c9032ed365f15adf5dcc8372318386
Alice <am171696@alunos.unisanta.br>
2020-12-15 09:58:14 -0300

    Adicionando classe, acao MostraEmpresa que possui o mesmo proposito que o servlet MostraEmpresa

---
+++ src/br/com/alura/gerenciador/acao/MostraEmpresa.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.BancoDeDados;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+public class MostraEmpresa {
+
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String id = request.getParameter("id");
+
+		BancoDeDados bancoDeDados = new BancoDeDados();
+		Empresa empresa = bancoDeDados.busca(Integer.valueOf(id));
+
+		RequestDispatcher rd = request.getRequestDispatcher("/formAlteraEmpresa.jsp");
+		request.setAttribute("empresa", empresa);
+		rd.forward(request, response);
+
+	}
+}
