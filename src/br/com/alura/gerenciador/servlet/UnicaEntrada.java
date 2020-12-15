97f2a2a995e83ced69b3a1f5d082e1f2d9f3cc0d
Alice <am171696@alunos.unisanta.br>
2020-12-15 09:25:13 -0300

    Delegando execucao das acoes

--- src/br/com/alura/gerenciador/servlet/UnicaEntrada.java
+++ src/br/com/alura/gerenciador/servlet/UnicaEntrada.java
@@ -7,18 +7,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UnicaEntrada
- */
+import br.com.alura.gerenciador.acao.ListaEmpresas;
+
 @WebServlet("/entrada")
 public class UnicaEntrada extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String acao = request.getParameter("acao");
+		
+		if(acao.equals("ListaEmpresas")) {
+			
+			ListaEmpresas listaEmpresas = new ListaEmpresas();
+			listaEmpresas.executa(request, response);
+			
+		} else if(acao.equals("RemoveEmpresa")) {
+
+		} else if(acao.equals("MostraEmpresa")) {
+			
+		}
+		
 	}
 
 }
