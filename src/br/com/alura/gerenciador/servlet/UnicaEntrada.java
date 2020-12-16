98098ebd85f62e5cf689d4594db9b67438318a5e
Alice <am171696@alunos.unisanta.br>
2020-12-16 12:05:32 -0300

    Refatorando o controlador. Retirando os ifs que chamam as acoes, gracas ao polimorfismo e a API reflection

--- src/br/com/alura/gerenciador/servlet/UnicaEntrada.java
+++ src/br/com/alura/gerenciador/servlet/UnicaEntrada.java
@@ -1,6 +1,7 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,12 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.alura.gerenciador.acao.AlteraEmpresa;
-import br.com.alura.gerenciador.acao.ListaEmpresas;
-import br.com.alura.gerenciador.acao.MostraEmpresa;
-import br.com.alura.gerenciador.acao.NovaEmpresa;
-import br.com.alura.gerenciador.acao.NovaEmpresaForm;
-import br.com.alura.gerenciador.acao.RemoveEmpresa;
+import br.com.alura.gerenciador.acao.Acao;
 
 @WebServlet("/entrada")
 public class UnicaEntrada extends HttpServlet {
@@ -25,35 +21,20 @@ public class UnicaEntrada extends HttpServlet {
 		String nome = null;
 
 		String paramAcao = request.getParameter("acao");
-
-		if (paramAcao.equals("ListaEmpresas")) {
-
-			ListaEmpresas acao = new ListaEmpresas();
+		
+		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
+		
+		try {
+			Class classe = Class.forName(nomeDaClasse);
+			Acao acao = (Acao) classe.getDeclaredConstructor().newInstance();
 			nome = acao.executa(request, response);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException | ServletException
+				| IOException e) {
 
-		} else if (paramAcao.equals("RemoveEmpresa")) {
-
-			RemoveEmpresa acao = new RemoveEmpresa();
-			nome = acao.executa(request, response);
-
-		} else if (paramAcao.equals("MostraEmpresa")) {
-
-			MostraEmpresa acao = new MostraEmpresa();
-			nome = acao.executa(request, response);
-
-		} else if (paramAcao.equals("AlteraEmpresa")) {
-
-			AlteraEmpresa acao = new AlteraEmpresa();
-			nome = acao.executa(request, response);
-		} else if (paramAcao.equals("NovaEmpresa")) {
-
-			NovaEmpresa acao = new NovaEmpresa();
-			nome = acao.executa(request, response);
-		} else if(paramAcao.equals("NovaEmpresaForm")) {
-			
-			NovaEmpresaForm acao = new NovaEmpresaForm();
-			nome = acao.executa();
+			throw new ServletException(e);
 		}
+		
 
 		String[] tipoEEndereco = nome.split(":");
 
