7fed0ad2be747595fa2c25c316527df91a7deb44
Alice <am171696@alunos.unisanta.br>
2020-12-16 12:07:24 -0300

    Implementando interface Acao e o sobrescrevendo metodo abstrato executa

--- src/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
+++ src/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
@@ -1,9 +1,16 @@
 package br.com.alura.gerenciador.acao;
 
-public class NovaEmpresaForm {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NovaEmpresaForm implements Acao{
 	
-	public String executa() {
-		
+	@Override
+	public String executa(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		return "forward:formNovaEmpresa.jsp";
 	}
 }
